3e9a91ce27ffe325377d5689a0e46a5189bb3c74
Totolescroc <thomas.clavel1@gmail.com>
2022-05-10 12:29:18 +0200

    reset css and link style.css

--- index.php
+++ index.php
@@ -33,6 +33,7 @@ $currentUsers =  getUrrentUser($user);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style/style.css">
     
     <title>Document</title>
 </head>
--- php/connexion.php
+++ php/connexion.php
@@ -50,6 +50,7 @@ if($_POST) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style/style.css">
     <title>Connexion</title>
 </head>
 <body>
